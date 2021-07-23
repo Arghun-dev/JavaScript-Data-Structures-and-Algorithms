69711e66bdf3f45629367374cfec3cd275e1d89a
arghun <arghun.developer@gmail.com>
2021-07-23 12:21:57 -0700

    implemented unique values

---
+++ UniqueValues.js
@@ -0,0 +1,14 @@
+function uniqueValues(arr) {
+    if (!arr.length) return 0;
+
+    let i = 0;
+    
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+
+    return i + 1;
+}
