2014157d92c9f00c28e052f41ad1c6f8e0dfb19a
arghun <arghun.developer@gmail.com>
2021-07-24 23:24:43 +0430

    implemented collectOddValues using recursive function

---
+++ collectOddValues.js
@@ -0,0 +1,17 @@
+function collectOddValus(arr) {
+    let result = [];
+
+    function helper(helperInput) {
+        if (!helperInput.length) return;
+
+        if (helperInput[0] % 2 !== 0) {
+            result.push(helperInput[0])
+        }
+
+        helper(helperInput.slice(1))
+    }
+
+    helper(arr)
+
+    return result;
+}
