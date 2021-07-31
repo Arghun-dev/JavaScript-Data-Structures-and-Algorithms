9d8de235c98af7f2f3b0ad747c2f04f454fb323f
arghun <arghun.developer@gmail.com>
2021-08-01 00:22:55 +0430

    optimized bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,24 +3,34 @@ function swap(arr, idx1, idx2) {
 }
 
 function bubbleSortAscend(arr) {
+    let noSwaps;
     for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
         for (let j = 0; j < i - 1; j++) {
             if (arr[j+1] < arr[j]) {
                 swap(arr, j, j+1);
+                noSwaps = false;
             }
         }
+
+        if (noSwaps) break;
     }
 
     return arr;
 }
 
 function bubbleSortDescend(arr) {
+    let noSwaps;
     for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
         for (let j = 0; j < i - 1; j++) {
             if (arr[j+1] > arr[j]) {
                 swap(arr, j, j+1);
+                noSwaps = false;
             }
         }
+
+        if (noSwaps) break;
     }
 
     return arr;
