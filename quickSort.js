95aacad07fc991f2ccd553fbc1c525ea7d9c0cb2
arghun <arghun.developer@gmail.com>
2021-08-05 15:11:38 +0430

    implemented quick sort

---
+++ quickSort.js
@@ -0,0 +1,35 @@
+// two main points of quick sort is that we use swap and recursion
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+    function swap(array, i, j) {
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    var pivot = arr[start];
+    var swapIndex = start;
+    for (var i = start + 1; i < arr.length; i++) {
+        if (pivot > arr[i]) {
+            swapIndex++;
+            swap(arr, swapIndex, i);
+        }
+    }
+
+    swap(arr, start, swapIndex);
+    return swapIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+        // left
+        quickSort(arr, left, pivotIndex - 1);
+        // right
+        quickSort(arr, pivotIndex+1, right);
+    }
+
+    return arr;
+}
+
+console.log(quickSort([59, 21, 5, 50, 209, 482]));
