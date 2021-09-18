37786d6aebce0e94b00c29a5fb44b858be21da41
Arghun-dev <a.mousanezhad@kian.digital>
2021-09-18 09:50:42 +0430

    implemented reverse method of singly linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -146,6 +146,22 @@ class SinglyLinkedList {
     this.length--;
     return removed;
   }
+
+  // reversing the linked list in place!
+  reverse() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var prev = null; 
+
+    for (var i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 var list = new SinglyLinkedList();
