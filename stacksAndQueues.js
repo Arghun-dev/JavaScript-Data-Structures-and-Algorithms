437446ea1144e16079edd5578cf4e4ce005de3e7
Arghun-dev <a.mousanezhad@kian.digital>
2021-09-26 09:32:17 +0330

    implementing stacks

--- stacksAndQueues.js
+++ stacksAndQueues.js
@@ -2,3 +2,16 @@
 
 // . Managing function invocations
 // . Undo / Redo
+// . Routing (the history object in browsers) is treated like a stack
+// In many frontend frameworks like react we'll use a stack to model the history that the pages you've viewed
+// . when we get to things like trees and graphs later on some of the algorithms that we'll be writing we actually use stacks and also queues
+
+
+// there is more than on way of implementing a stack
+
+// ... easiest way is to implement stacks are arrays and use push and pop and shift and unshift methods of array JavaScript does not. 
+
+
+//  first step implementation of stacks using Linked List
+
+// Wrting Our own Stack from scratch
