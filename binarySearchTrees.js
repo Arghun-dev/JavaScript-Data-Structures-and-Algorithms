05e76ad04e50ea2f8df3f394c51db4da48ac9371
Arghun-dev <a.mousanezhad@kian.digital>
2021-09-28 17:47:51 +0330

    implemeneted binary search tree

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -55,3 +55,23 @@
 // . Every parent node has at most two children
 // . Every node to the left of a parent node is always less than the parent
 // . Every node to the right of a parent node is always greater than parent
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null; 
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
