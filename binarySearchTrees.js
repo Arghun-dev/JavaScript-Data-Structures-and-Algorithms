ee0ffc4fdd68d070d8ced1e43f710a085f890a80
Arghun-dev <a.mousanezhad@kian.digital>
2021-09-28 16:28:38 +0330

    introducted trees data structure

---
+++ binarySearchTrees.js
@@ -0,0 +1,24 @@
+// Trees are a classic data structure and they're also very commonly used.
+
+// What is a Tree?
+
+// a data structure that consists of nodes in parent / child relationship
+
+// Lists => linear => it's one thing and then the next and next ... everything is in a line => we have just one line path
+// Trees => nonLinear => they are branches => in trees we have different paths
+
+// so you can think of a SinglyLinkedList as a special case of a tree
+
+// when we talk about trees, there are some rules
+
+// 1. A node can only point to a child, parent child relationship, we can't have a child pointing to the parent, we can't have a node pointing to sibling
+
+
+// TREE Terminology
+
+// . Root - The top node in a tree
+// . Child - A node directly connected to another node when moving away from the Root
+// . Parent - The converse notion of a child
+// . Siblings - A group of nodes with the same parent
+// . Leaf - A node with no children
+// . Edge - The connection between one node and another
